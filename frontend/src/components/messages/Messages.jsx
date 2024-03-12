681ae7fea57a8707fa5a1cd72f867caed13552a7
saitejadamaraju <saitejadamaraju4@gmail.com>
2024-03-12 22:37:51 +0530

    frontend and linking frontend and backend completed

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,22 @@
+import Message from "./Message";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+import useGetMessages from "../../hooks/useGetMessages";
+
+const Messages = () => {
+
+	const { messages, loading } = useGetMessages();
+
+	return (
+		<div className='px-4 flex-1 overflow-auto'>
+            
+			{!loading && messages.length>0 && 
+				messages.map((message)=><Message key={message._id} message={message} />)}
+
+			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className='text-center text-white'>Send a message to start the conversation</p>
+			)}
+		</div>
+	);
+};
+export default Messages;
