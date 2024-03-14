c80a5d11e548952277ebdc63f2a4f1c4dc139fd6
saitejadamaraju <saitejadamaraju4@gmail.com>
2024-03-14 12:10:47 +0530

    changes in server.js file for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
@@ -9,6 +10,8 @@ import {app,server} from './socket/socket.js';
 
 //const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -24,6 +27,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res)=>{
+
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
 server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`server is running in port ${PORT}`);
